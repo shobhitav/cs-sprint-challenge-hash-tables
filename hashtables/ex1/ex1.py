f0c9047499df380cf68c30ec5d41a222433ef96f
Shobhita Vaishnav <to.shobhita@gmail.com>
2020-07-19 16:42:53 -0700

    ex1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,22 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    
+    length=len(weights)
+    cache={}
+    # populate the cache with values and indices from weights
+    for i in range(len(weights)):
+        cache[weights[i]] = i
+    print(cache) 
 
-    return None
+    # look up index from weights list and corresponding value in cache for key which 
+    # satisfies condition (limit - weight)
+    for index,weight in enumerate(weights):
+        w=limit-weight
+        if w in cache :
+            return (index,cache[w]) if index > i else (cache[w],index)
+
+      
+
+
+print(get_indices_of_item_weights([ 4, 6, 10, 15, 16 ],5,21))
