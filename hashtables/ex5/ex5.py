15f97e4e80a0b2c38e0f54681474fe9cd71077bc
Shobhita Vaishnav <to.shobhita@gmail.com>
2020-07-19 18:59:37 -0700

    ex5 done

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,22 @@
 # Your code here
 
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache={}
+    result=[]
+    for file in files:
+        # if key doesn't exist, initialize with empty list and append the file full path
+        cache.setdefault(file.split('/')[-1],[]).append(file)
+        
+    # print(cache)
 
+    for item in queries:
+        if item in cache:
+            for val in cache[item]:
+                result.append(val)
+     
     return result
 
 
@@ -15,7 +24,8 @@ if __name__ == "__main__":
     files = [
         '/bin/foo',
         '/bin/bar',
-        '/usr/bin/baz'
+        '/usr/bin/baz',
+        '/dir/bin/baz'
     ]
     queries = [
         "foo",
